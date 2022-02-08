02623b06a08378b0baa04ac82cdda65611406d31
shenkaibo <kaibo@fit2cloud.com>
2022-02-08 10:42:54 +0800

    fix:支持按照 模块层级  创建

--- backend/src/test/java/io/metersphere/Test.java
+++ backend/src/test/java/io/metersphere/Test.java
@@ -3,6 +3,7 @@ package io.metersphere;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import io.metersphere.api.dto.ApiTestImportRequest;
+import io.metersphere.api.dto.automation.parse.ScenarioImport;
 import io.metersphere.api.dto.automation.parse.ScenarioImportParserFactory;
 import io.metersphere.api.dto.definition.request.MsScenario;
 import io.metersphere.api.dto.definition.request.sampler.MsHTTPSamplerProxy;
@@ -54,7 +55,14 @@ public class Test{
         MsScenario msScenario = new MsScenario();
         msScenario.setName(info.getName());
         //this.projectId = request.getProjectId();
-        parseItem(postmanCollection);
+        ScenarioImport scenarioImport = new ScenarioImport();
+
+        ApiScenarioWithBLOBs apiScenarioWithBLOBs1 = new ApiScenarioWithBLOBs();
+        apiScenarioWithBLOBs1.setModulePath("/dev");
+        apiScenarioWithBLOBs1.setApiScenarioModuleId("47ba7690-1424-4153-9916-016de5b929cb");
+        apiScenarioWithBLOBs1.setProjectId("fa34f2dd-c959-4f29-bd38-64acfc701fce");
+        apiScenarioWithBLOBs1.setUserId("kaibo");
+        parseItem(postmanCollection,apiScenarioWithBLOBs1,apiScenarioWithBLOBs1.getProjectId(),apiScenarioWithBLOBs1.getUserId());
 
 
         System.out.println();
@@ -84,7 +92,7 @@ public class Test{
         return testStr.toString();
     }
 
-    private void parseItem(PostmanCollection postmanCollection) throws FileNotFoundException {
+    private void parseItem(PostmanCollection postmanCollection,ApiScenarioWithBLOBs apiScenario,String projectId,String userId) throws FileNotFoundException {
         List<PostmanCollection> postmanCollectionChildList = new ArrayList<>();
         Iterator<PostmanItem> it = postmanCollection.getItem().iterator();
         while(it.hasNext()){
@@ -112,18 +120,19 @@ public class Test{
 
         ApiTestImportRequest request = new ApiTestImportRequest();
         request.setPlatform("Postman");
-        request.setModuleId("47ba7690-1424-4153-9916-016de5b929cb");
-        request.setModulePath("/dev");
-        request.setProjectId("fa34f2dd-c959-4f29-bd38-64acfc701fce");
-        request.setUserId("kaibo");
+        request.setModuleId(apiScenario.getApiScenarioModuleId());
+        request.setModulePath(apiScenario.getModulePath());
+        request.setProjectId(projectId);
+        request.setUserId(userId);
         //request.set
         //执行导入逻辑
-        apiAutomationService.scenarioImport2(fos, request);
+        ScenarioImport scenarioImport = apiAutomationService.scenarioImport2(fos, request);
         //
+        ApiScenarioWithBLOBs scenario1 = scenarioImport.getData().get(0);
 
         //递归调用
         for(PostmanCollection postmanCollection1 : postmanCollectionChildList){
-            parseItem(postmanCollection1);
+            parseItem(postmanCollection1,scenario1,projectId,userId);
         }
 
     }
